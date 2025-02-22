1680c3777b3dbf50b646c0b3902b0bbc2af0b2bc
조상윤 <106017370+Joseng8908@users.noreply.github.com>
2025-02-22 14:59:20 +0000

    chapter2

--- ftlog/calculator/calculator.go
+++ ftlog/calculator/calculator.go
@@ -4,7 +4,14 @@ package calculator
 // Add takes two numbers and returns the result of adding
 // them together
 
-func Add(a, b float64) float64{
+func Add(a, b float64) float64 {
 	return a + b
 }
 
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
--- ftlog/calculator/calculator_test.go
+++ ftlog/calculator/calculator_test.go
@@ -7,8 +7,49 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 6, b: 3, want: 9},
+	}
+	for _, tc := range testCases{
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f) : want %f, got %f", tc.a, tc.
+			b, tc.want, got)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 3, b: 5, want: -2},
+		{a: 5, b: 2, want: 3},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f) : want %f, got %f", tc.a, 
+			tc.b, tc.want, got)
+		}
+	}
+}
+
+
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
 	var want float64 = 4
-	got := calculator.Add(2, 2)
+	got := calculator.Multiply(2, 2)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
