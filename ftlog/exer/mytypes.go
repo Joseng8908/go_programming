b28cf08e2cb99bd3547e8e3cc7759e59c74b8782
조상윤 <106017370+Joseng8908@users.noreply.github.com>
2025-04-11 14:36:36 +0000

    2025_04_11

--- ftlog/exer/mytypes.go
+++ ftlog/exer/mytypes.go
@@ -1,7 +1,17 @@
 package mytypes
 
+import "strings"
+
 type MyInt int
+type MyIntPointer *int
 type MyString string
+type MyBuilder struct {
+	Contents strings.Builder
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
 
 func (i MyInt) Twice() MyInt {
 	return i * 2
@@ -9,4 +19,16 @@ func (i MyInt) Twice() MyInt {
 
 func (s MyString) Len() int {
 	return len(s)
+}
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+func (su StringUppercaser) ToUpper() string {
+	return strings.ToUpper(su.Contents.String())
+}
+
+func (input *MyInt)Double() {
+	*input *= 2
 }
