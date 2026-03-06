7d7f8429bc4955d1f3889f7d221de48de4dde3f2
조상윤 <sengyun8908@naver.com>
2026-03-06 17:37:01 +0900

    feat(main.go): 실행 로직 조합 1. Args로 인자를 받아서 파싱 2. container 패키지 import 후 NewConainer 명령어로 어떤 특정 인자(프로세스 ex)bin/sh) 를 실행 3. 실행이 아닌 일단 Container를 생성, namespace개념을 이용하여 호스트, PID, 파일시스템을 지정해서 만듦 4. continaer 패키지의 Start로 패키지 실행 5. 인자에 있는 명령어가 실행 후 프로그램 종료
    Issue #01: 명령어가 실행되지 않고 바로 종료되는 오류가 있음

---
+++ micro_container/go.mod
@@ -0,0 +1,3 @@
+module micro_container
+
+go 1.22.2
---
+++ micro_container/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"micro_container/container"
+)
+
+func main() {
+	// 사용법 설명: go run main.go <command> <args...>
+	// ex) go run main.go /bin/sh
+	if len(os.Args) < 2 {
+		fmt.Println("사용법: go run main.go <command>")
+		return
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	fmt.Printf("새로운 컨테이너 생성중,,,%s", cmd)
+
+	// 컨테이너 생성
+	c := container.NewContainer("1", cmd, args...)
+
+	// 컨테이너 실행
+	if err := c.Start(); err != nil {
+		fmt.Printf("컨테이너 실행 실패: %v\n", err)
+		return
+	}
+
+	// 프로세스 대기
+	// 커테이너 프로세스각 끝나야 끝남
+	err := c.Cmd.Wait()
+	if err != nil {
+		fmt.Printf("컨테이너가 에러남: %v\n", err)
+	} else {
+		fmt.Println("컨테이너 종료")
+	}
+}
